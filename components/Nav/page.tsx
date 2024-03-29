ad248f80ce564ea29ae75a06aa2ede2bd37ddb6a
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-03-29 19:58:44 +0000

    Update Nav component styling

--- components/Nav/page.tsx
+++ components/Nav/page.tsx
@@ -6,17 +6,17 @@ function Nav() {
     <>
       <div className="bg-slate-400">
         <nav className="bg-slate-900  max-w-[1080px] mx-auto h-[70px]">
-          <div className="flex">
+          <div className="flex justify-between items-center lg:mx-0 mx-5">
             <div className="flex">
               <img src={LogoSrc.src} alt="Logo" width={50} />
-              <p className="ml-3 text-lg ">King</p>
+              <p className="ml-3 text-lg content-center ">King</p>
             </div>
 
-            <div>
-              <button className="border border-white h-10 w-20 rounded-lg">
+            <div className="flex items-center">
+              <button className="border border-white h-10 w-10 rounded-lg text-xl">
                 +
               </button>
-              <button className="border border-white h-10 w-20 rounded-lg">
+              <button className="border border-white h-10 w-20 rounded-lg ml-2">
                 Account
               </button>
             </div>
