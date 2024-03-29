615b24336b6690457ef21d6137dbe741e56cf329
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-03-29 20:00:14 +0000

    Add custom colors to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "nav-bar" : "#3a3a3c",
+        "main-body" : "#222224",
+      },
     },
   },
   plugins: [],
